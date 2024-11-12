97344cb94b46e5c88269427e7b553b4f8fe1ea1e
YagoCastro <yhagocastro9@gmail.com>
2024-11-12 20:53:14 -0300

    Test-update

--- ComputationalIntelligence/HW1/1.content_base/custom_css_preprocessor.py
+++
@@ -1,6 +0,0 @@
-from nbconvert.preprocessors import Preprocessor
-
-class CustomCSSPreprocessor(Preprocessor):
-    def preprocess(self, nb, resources):
-        resources['inlining'] = {'css': open(r"C:\Users\Yago\Desktop\GitHubLocal\UFC\ComputationalIntelligence\HW1\custom.css").read()}
-        return nb, resources
